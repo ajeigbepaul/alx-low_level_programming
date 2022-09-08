649bf3bdbba43d48138f935953c11f3b24f13a31
ajeigbepaul <ajeigbepaul@yahoo.com>
2022-09-08 14:11:44 -0700

    a

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 /**
-* main - print a string with printf
-*
-* Description: Write a C program that prints exactly with proper grammar,
-* but the outcome is a piece of art,, followed by a new line
-* Return: 0
-*/
+ * * main - print a string with printf
+ * *
+ * * Description: Write a C program that prints exactly with proper grammar,
+ * * but the outcome is a piece of art,, followed by a new line
+ * * Return: 0
+ * */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return(0);
 }
-
