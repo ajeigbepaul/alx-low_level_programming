cac139575abd9627c8d926ecedff8842ba3951a2
ajeigbepaul <ajeigbepaul@yahoo.com>
2022-09-09 16:49:38 -0700

    a

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -4,7 +4,8 @@
 /**
  * main - print the digit of number from 0 to 10
  *
- * Description: Write a program that prints all single digit numbers of base 10 
+ * Description: Write a program that prints all
+ * single digit numbers of base 10
  * starting from 0, followed by a new line.
  * Return: 0
  *
@@ -13,7 +14,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 0; i < 10; i++)
 		putchar(i);
 	putchar('\n');
 	return (0);
