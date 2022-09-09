bda08b9baa103539ab01323e15f60bc780858071
ajeigbepaul <ajeigbepaul@yahoo.com>
2022-09-09 14:58:26 -0700

    b

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,13 +2,15 @@
 /**
  * main - print characters in both lowercase and uppercase
  *
- * Description: Write a program that prints the alphabet in lowercase, and then in uppercase
+ * Description: Write a program that prints the
+ * alphabet in lowercase, and then in uppercase
  * followed by a new line
  * Return: 0
 */
 int main(void)
 {
 	char ch;
+
 	for (ch = 'a' ; ch <= 'z' ; ch++)
 		putchar(ch);
 	for (ch = 'A' ; ch <= 'Z' ; ch++)
