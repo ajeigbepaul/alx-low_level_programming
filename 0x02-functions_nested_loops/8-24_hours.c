dc7f90a4590e49cdf4851ec2c77722e103cfcfe0
ajeigbepaul <ajeigbepaul@yahoo.com>
2022-09-13 15:10:23 -0700

    hot

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -20,9 +20,9 @@ void jack_bauer(void)
 				_putchar(':');
 				_putchar('0');
 				_putchar(j);
-			else if (j > 10)
+			else if (j >= 10)
 				_putchar(':');
 				_putchar((j / 10) + '0');
 				_putchar((j % 10) + '0');
-	_putchar('\n');
+			_putchar('\n');
 }
