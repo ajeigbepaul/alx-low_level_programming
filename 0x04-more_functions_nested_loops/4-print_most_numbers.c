1455e625a197a41140cf1f9f9cfdb696fb570a9c
ajeigbepaul <ajeigbepaul@yahoo.com>
2022-09-15 06:13:19 -0700

    a

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,18 +8,15 @@
 */
 void print_most_numbers(void)
 {
-	int x = 0;
+	int x;
 
-	for (; x <= 9; x++)
+	for (x = 48; x <= 58; x++)
 	{
-		if (x == 2 || x == 4)
+		if ((x == 50) || (x == 52))
 		{
 			continue;
 		}
-		else
-		{
-			_putchar(x + '0');
-		}
+		putchar(x);
 	}
-	_putchar('\n');
+	putchar(10);
 }
