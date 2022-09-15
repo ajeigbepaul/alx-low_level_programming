035d5706fcd047aca9ac86e29c1b0ab50add8841
ajeigbepaul <ajeigbepaul@yahoo.com>
2022-09-15 05:44:16 -0700

    add

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - Print from 0 to 9
+ *
+ * Description: Function print from zero to nine
+ * Return: 0
+ *
+*/
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+		_putchar(a);
+	_putchar('\n');
+}
