9b67652bca1c51855af3fb3c06313a7cba0c4f69
ajeigbepaul <ajeigbepaul@yahoo.com>
2022-09-15 05:55:11 -0700

    a

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,17 +3,17 @@
  * print_numbers - Print from 0 to 9
  *
  * Description: Function print from zero to nine
- * Return: 0
+ * Return: returns nothing
  *
 */
 void print_numbers(void)
 {
-	int x;
+	int n;
 
-	for (x = 48; x < 58; x++)
+	for (n = 48; n < 58; n++)
 	{
-		_putchar(x);
+		putchar(x);
 	}
 
-	_putchar('\n');
+	putchar('\n');
 }
