b42e75a766c095a1c355bafd6d0c485b88b3b0a3
ajeigbepaul <ajeigbepaul@yahoo.com>
2022-09-16 05:39:20 -0700

    a

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,16 +6,39 @@
 */
 void print_number(int n)
 {
-	unsigned int n1 = n;
+	char ld, cr;
+	int r;
+	int ct = 0;
 
 	if (n < 0)
 	{
 		_putchar('-');
-		n1 = -n1;
+		ld = (char)('0' - (n % 10));
+		n = n / -10;
 	}
-	if ((n1 / 10) > 0)
+	else
 	{
-		print_number(n1 / 10);
+		ld = (char)((n % 10) + '0');
+		n = n / 10;
 	}
-	_putchar((n1 % 10) + '48');
+	r = 0;
+	while (n > 0)
+	{
+		r = r * 10 + (n % 10);
+		n = n / 10;
+		ct++;
+	}
+	while (r > 0)
+	{
+		cr = (char)((r % 10) + '0');
+		_putchar(cr);
+		r = r / 10;
+		ct--;
+	}
+	while (ct != 0)
+	{
+		_putchar('0');
+		ct--;
+	}
+	_putchar(ld);
 }
