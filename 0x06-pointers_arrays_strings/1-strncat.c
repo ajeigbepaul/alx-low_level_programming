ebcda2b9871ade9a2e7eed96317b6cdd96e5eaf5
ajeigbepaul <ajeigbepaul@yahoo.com>
2022-09-21 16:37:25 -0700

    a

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,11 +13,11 @@ char *_strncat(char *dest, char *src, int n)
 
 	while (dest[index++])
 	{
-		dest_len++
+		dest_len++;
 	}
 	for (index = 0; src[index] && index < n; index++)
 	{
 		dest[dest_len++] = src[index];
 	}
-	return (dest)
+	return (dest);
 }
