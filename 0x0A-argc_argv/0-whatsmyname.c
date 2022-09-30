be73ed19cb2b755c7660f638c5e0f8a4299dedbf
ajeigbepaul <ajeigbepaul@yahoo.com>
2022-09-30 13:18:07 -0700

    a

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print the name of the programe
+ * @argc: count arguments
+ * @argv: Arguments
+ *
+ * Return: Always 0
+*/
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ *  *main - header file for protoypes
+ *void protoypes (void)
+ *int protoypes (int)
+ */
+int _putchar(char c);
+int check_num(char *str);
+#endif /* MAIN_H */
