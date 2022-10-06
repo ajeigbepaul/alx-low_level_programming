fec81df03e153a663fd51bebf6feaabf98bb80f6
ajeigbepaul <ajeigbepaul@yahoo.com>
2022-10-06 07:30:54 -0700

    hot

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ *  *main - header file for protoypes
+ *void protoypes (void)
+ *int protoypes (int)
+ */
+int _putchar(char c);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *malloc_checked(unsigned int b);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+#endif /* MAIN_H */
